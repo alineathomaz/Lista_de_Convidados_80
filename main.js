a020aee06ddb86e092c43228e98e7f556095a06c
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2022-05-02 21:05:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,74 +1,47 @@
-listaConvidadosArray = [];
+var listaConvidadosArray = [];
 
 function submit() //função submit (enviar) um nome na Lista:
 {
     var displayConvidadoArray = [];
-    for (var i = 1; i <= 100 ; i++) //loop para inserir até 100 convidados
-    {
-        var nomeConvidado = document.getElementById("nomeConvidado"+i).value;
-        console.log(nomeConvidado);
-        listaConvidadosArray.push(nomeConvidado);  //push() serve para adicionar um nome à lista de convidados
-    }
-
-    console.log(listaConvidadosArray);
-
+    var nomeConvidado = document.getElementById("nomeConvidado").value;
+    listaConvidadosArray.push(nomeConvidado);  //push() serve para adicionar um nome à lista de convidados
     var lengthConvidadosArray = listaConvidadosArray.length;
-    console.log(lengthConvidadosArray);
 
-    for (var k = 0; k < lengthConvidadosArray; k++) 
-    {
-        displayConvidadoArray.push("<h4>NAME - "+ listaConvidadosArray[k] + "</h4>");
-        console.log(displayConvidadoArray);
-    }
-
-    console.log(displayConvidadoArray);
-    document.getElementById("displayNameWithCommas").innerHTML = displayConvidadoArray;
-
+    document.getElementById("displayNameWithCommas").innerHTML = listaConvidadosArray;
     var removeCommas = displayConvidadoArray.join(" ");
-    console.log(removeCommas);
     document.getElementById("displayNameWithoutCommas").innerHTML = removeCommas;
 
-
-    document.getElementById("submitButton").style.display = "none";
     document.getElementById("sortButton").style.display = "inline-block";
 }
 
 function sorting() //função sorting (organizar)
 {
     listaConvidadosArray.sort(); //sort() serve para organizar a lista de convidados
-    console.log(listaConvidadosArray);
-
     var displayConvidadoArraySorting = [];
 
     var lengthConvidadosArray = listaConvidadosArray.length;
-    console.log(lengthConvidadosArray);
-
     for (var k = 0; k < lengthConvidadosArray; k++) 
     {
-        displayConvidadoArraySorting.push("<h4>NAME - " + listaConvidadosArray[k] + "</h4>");
-        console.log(displayConvidadoArraySorting);
+        displayConvidadoArraySorting.push("<h4>Convidado(a): " + listaConvidadosArray[k] + "</h4>");
     }
 
     var removeCommas = displayConvidadoArraySorting.join(" ");
-    console.log(removeCommas);
 
-    document.getElementById("displayNameWithoutCommas").innerHTML = removeCommas;
+    document.getElementById("displayNameWithoutCommasSorting").innerHTML = removeCommas;
 }
 
 
 
-function searching() //função para search (pesquisar) um nome na lista
+function searching() //função para search (pesquisar) um nome na lista, a pesquisa só será válida se buscar o nome completo do convidado conforme foi cadastrado
 {
     var s = document.getElementById("s1").value;
     var found = 0;
-    var j;
-    for (j=0; j<nomeConvidado.length; j++)
-    {
-        if (s == nomeConvidado[j])
-        {
-            found = found+1;
-        }
-    }
+  
+    listaConvidadosArray.forEach(element => {
+       if(element == s){
+       found++;
+       }
+    });
+
     document.getElementById("p2").innerHTML="Nome encontrado "+found+" vez(es)";
-    console.log("Nome encontrado "+found+" vez(es)");
 }
